74f4189e28d309ef6427581dd0602b5ffb67b76b
JHY <jaihuayen@qnap.com>
2022-11-05 23:18:45 +0800

    🚧 Add model class code and some un-finished code

---
+++ train.py
@@ -0,0 +1,13 @@
+import os
+from config import get_config
+
+args = get_config()
+os.environ["CUDA_VISIBLE_DEVICES"] = args.gpu_id
+
+import random
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import warnings
+warnings.filterwarnings('ignore')
